f2c86654a6055c4b67e9d2aa99bbf5fad8fc8451
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-07-17 15:27:22 -0600

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,14 @@
-document.write("Hello, World!");
+var A = "This is a story" + " of a girl!<br>", B="Candy",C="Jester",D="Apple Bottom Jeans";
+var A=A.fontcolor("red");
+var B="Window";
+var B=B.fontcolor("orange");
+var C="Pane";
+var C=C.fontcolor("yellow");
+var D=D.fontcolor("green");
+document.write(A);
+document.write("She said, \"You\'ll be sorry!\" to the mean kid. <br>"
++ "That was the last time she ever saw him.<br>");
+document.write(15+25);
+var prom="This is an Alert";
+window.alert(prom);
+document.write(B+C);
