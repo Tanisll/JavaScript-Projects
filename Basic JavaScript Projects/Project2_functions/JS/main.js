30be83d46e0ae408b7a46f78c7016f8c4d79d4ed
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-07-18 22:13:36 -0600

    Project 2 assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+function Make_Blue() {
+    var str = "this should be blue";
+    var result = str.fontcolor("blue");
+    document.getElementById("Blue_A") .innerHTML = result;
+}
+
+var A = $5;
+var B = $10;
+var C = A+B;
+var D = A+B*0.8;
+
+window.alert("Save on Budle Deal! Buy " document.write(C) "and get it for " document.write(D) "!!!")
+
