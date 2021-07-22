dc4dd55791863849ccaf57696bf3179f464972cf
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-07-21 22:57:44 -0600

    alarm clock and slide show
    -moved alarm clock from P8 to P9 and added a slide show to P9.

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,41 @@
+function countdown(){
+    var seconds=document.getElementById("seconds").value;
+
+    function tick(){
+        seconds=seconds-1;
+        timer.innerHTML=seconds;
+        setTimeout(tick,1000);
+    if(seconds==-1){
+        alert("Time's up!");
+    }
+    }
+    tick();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
