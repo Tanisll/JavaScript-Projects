61048d9215d9e3ffb1779ea846663d0f75bfe98b
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-07-21 22:22:30 -0600

    project 8

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -14,3 +14,23 @@ function search(){
     var A = "This is how we clean our room"
     document.getElementById("search").innerHTML=A.search("clean");
 }
+
+function string_Method(){
+    var X=182;
+    document.getElementById("Numbers_to_string").innerHTML=X.toString();
+}
+
+function precision_Method(){
+    var X=12938.3216549874561;
+    document.getElementById("Precision").innerHTML=X.toPrecision(6);
+}
+
+function fixed_Method(){
+    var X=12938.3216549874561;
+    document.getElementById("Fixed").innerHTML=X.toFixed(1);
+}
+
+function value_Method(){
+    var X=12938.3216549874561;
+    document.getElementById("value").innerHTML=X.valueOf();
+}
