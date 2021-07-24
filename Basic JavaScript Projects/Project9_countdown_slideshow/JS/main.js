04ce7e71bb4070d3d8f72f64c3406c957a53122a
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-07-24 16:42:34 -0600

    Update main.js
    removed automatic slide advancing and made it manual. I couldn't figure out how to make the next and previous slide buttons to work when it advanced automatically

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -25,17 +25,18 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 8000); // Change image every 8 seconds
+  dots[slideIndex-1].className += " active";
 }
